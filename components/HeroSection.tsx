26801f5a4ee36cc02304a7db5048d75326e575fd
Vinicius2026 <jasvinigaming@gmail.com>
2026-04-11 04:09:23 +0000

    feat: novas atualizações dashboard18h8b

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -83,6 +83,6 @@ export default function HeroSection() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
                 </svg>
             </div>
-        </main>
+        </section>
     );
 }
